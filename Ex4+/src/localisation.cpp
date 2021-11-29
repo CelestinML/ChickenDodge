0178adda51ee787ee03ca3a2e301c51247730548
CelestinML <celestin.moenne@gmail.com>
2021-11-28 20:20:25 -0500

    comments + update readme

--- Ex4+/src/localisation.cpp
+++ Ex4+/src/localisation.cpp
@@ -37,16 +37,25 @@ namespace SimpleGE
 
     std::string result = localized->second;
 
+    //On cherche les clés à utiliser pour trouver la bonne traduction
+    //Pour celan on créer un pattern de recherche contenant une accolade ouvrante, 
+    //un mot constitué d'un certain nombre de caractères, et une accolade fermante
     std::regex search("\\{([^\\}]*)\\}");
     std::smatch match;
+    //L'offset nous permettra de ne pas avoir à rechercher dans l'intégralité du mot à chaque boucle
     std::size_t offset = 0;
     while(std::regex_search(result.cbegin() + offset, result.cend(), match, search))
     {
+      //On a trouvé un mot à remplacer
+      //On récupère la traduction dans la langue actuelle
       std::string new_text = mergedContext.at(match.str(1));
+      //On remplace le pattern trouvé (de l'accolade ouvrante à l'accolade fermante) par la traduction
       result.replace(match[0].first, match[0].first + match[0].length(),new_text);
+      //On augmente l'offset pour ne pas re chercher dans l'intégralité du texte
       offset += match.position(0) + new_text.length();
     }
 
+    //Une fois qu'on a trouvé et remplacé tous les patterns, on retourne enfin le résultat
     return result;
   }
 } // namespace SimpleGE
